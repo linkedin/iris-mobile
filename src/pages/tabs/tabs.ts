bdc4477da33c0e367fd71cb57ab2166d01d7bac7
Diego Cepeda <dcepeda@linkedin.com>
2019-07-22 17:01:47 -0700

    fix tab highlighting bug

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Tabs } from 'ionic-angular';
 import { IncidentsPage } from '../incidents/incidents';
 import { OncallPage } from '../oncall/oncall';
 import { OncallMePage } from '../oncall-me/oncall-me';
@@ -10,13 +10,29 @@ import { OncallMePage } from '../oncall-me/oncall-me';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
+  @ViewChild('navTabs') tabRef: Tabs;
+
   rootPage:any = IncidentsPage;
   oncallPage:any = OncallPage;
   mePage:any = OncallMePage;
+  highlight: {left: string, top: string, width: string}
+  isTab: boolean = false
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+  }
+
+  selectedTab() {
+    let thisTab = this.tabRef.getSelected()
+    let fbox = thisTab.btn.getNativeElement().getBoundingClientRect()
+    this.highlight = {
+      left: fbox.left+'px',
+      top: fbox.bottom-2+'px',
+      width: fbox.width+'px'
+    }
+    this.isTab = true
   }
 
 }
